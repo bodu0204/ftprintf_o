49ad4f962cecfe3b9f9b71892ec8386996371ec6
呂明 <ryoakira@ryoakiranoMacBook-Pro.local>
2022-03-08 18:30:24 +0900

    commit form makefile

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -2,6 +2,10 @@
 #include <unistd.h>
 
 enum
+{
+	OUTPUT,
+	PUTLEN,
+};
 
 int	ft_printf(const char *	fmt, ...)
 {
@@ -18,11 +22,40 @@ int	ft_printf(const char *	fmt, ...)
 		len = 0;
 		while (fmt[len] && fmt[len] != '%')
 			len++;
-		if (output(fmt, len, ap, PUT))
+		if (output(fmt, len, ap))
 			return (-1);
 		fmt += len;
 		if (*fmt != '%' && rsp(fmt, ap, &len))
 			return (-1);
 	}
-	return (rtn);
+	return (output(NULL, 0, ap));
+}
+
+int	output(const char *	str, size_t len, va_list	ap)
+{
+	static size_t	putlen = 0;
+	int flag;
+
+	if (!str)
+	{
+		va_end(ap);
+		return((int)putlen);
+	}
+	putlen += len;
+	#ifdef REAL
+		while (len)
+		{
+			flag = -1 * !putchar(*str);
+			str++;
+			len--;
+		}
+	#else
+		flag = write(1, str, len);
+	#endif
+	if (flag == -1)
+	{
+		va_end(ap);
+		return (1);
+	}
+	return (0);
 }
