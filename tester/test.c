b096e2ce32ad85f8dbfb4607e9aac0950dd30cfa
Bodu Lyu <blyu@c1r29s9.42tokyo.jp>
2022-03-15 17:03:28 +0900

    commit form makefile

--- tester/test.c
+++ tester/test.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+#include "libftprintf.h"
+
+int main (void)
+{
+	int i;
+
+	printf("ori\n");
+	i = printf("i\n");
+	printf("\nreturn = %d\n", i);
+	printf("\n");
+	printf("your\n");
+	i = ft_printf("i\n");
+	printf("\nreturn = %d\n", i);
+	return (0);
+}
 
