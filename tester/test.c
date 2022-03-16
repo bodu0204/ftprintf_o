7bb34729f20c589d83f7c89a45be418e3567f714
Bodu Lyu <blyu@c1r29s9.42tokyo.jp>
2022-03-16 14:55:09 +0900

    commit form makefile

--- tester/test.c
+++ tester/test.c
@@ -6,11 +6,11 @@ int main (void)
 	int i;
 
 	printf("ori\n");
-	i = printf("apple, %%d = %-10d, %%s = %s\n", 63, "test");/*  */
+	i = printf("apple, %%d = %-10d, %%s = %.80s\n", 63, "test");/*  */
 	printf("\nreturn = %d\n", i);
 	printf("\n");
 	printf("your\n");
-	i = ft_printf("apple, %%d = %-10d, %%s = %s\n", 63, "test");/*  */
+	i = ft_printf("apple, %%d = %-10d, %%s = %.80s\n", 63, "test");/*  */
 	printf("\nreturn = %d\n", i);
 	return (0);
 }
