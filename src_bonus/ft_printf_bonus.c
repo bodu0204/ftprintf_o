27d2986da823e2d187f1476c6d91c9d9e6acd0d4
Bodu Lyu <blyu@c1r29s9.42tokyo.jp>
2022-03-16 14:52:43 +0900

    commit form makefile

--- src_bonus/ft_printf_bonus.c
+++ src_bonus/ft_printf_bonus.c
@@ -1,39 +1,3 @@
-void show(char* s)
-{
-    size_t i;
-    size_t ii;
-    size_t m;
-    size_t line;
-
-    line = 32;
-    m = 0;
-    ii = 0;
-    printf("\nstart:%p\n", s);
-    while (m < 3)
-    {
-        printf("|");
-        i = 0;
-        while (i < line)
-        {
-            printf("%7zu|", ii + i + 1);
-            i++;
-        }
-        printf("\n|");
-        i = 0;
-        while (i < line)
-        {
-            if (*(s + ii + i) == 0 && !m)
-                m++;
-            printf("%4d(%c)|", (int)*(s + ii + i), (*(s + ii + i) > 32 && *(s + ii + i) < 127) ? *(s + ii + i) : 32);
-            i++;
-        }
-        printf("\n");
-        if (m > 0)
-            m++;
-        ii += line;
-    }
-}
-
 #include "ft_printf_bonus.h"
 
 int	ft_printf(const char	*fmt, ...)
