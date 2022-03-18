b8949f31b8fa3b7d17343ad9c133eeed0ec8971e
呂明 <ryoakira@ryoakiranoMacBook-Pro.local>
2022-03-18 09:33:10 +0900

    commit form makefile

--- src_bonus/tool_bonus.c
+++ src_bonus/tool_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ryoakira <ryoakira@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/16 17:48:55 by blyu              #+#    #+#             */
-/*   Updated: 2022/03/18 09:17:03 by ryoakira         ###   ########.fr       */
+/*   Updated: 2022/03/18 09:33:03 by ryoakira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,7 @@ size_t	blclen(t_block	*b)
 	size_t	len;
 
 	len = b->spase;
-	len += ft_strlen(b->sing);
+	len += b->singl;
 	len += b->zero;
 	len += b->numl;
 	len += b->fmtl;
@@ -59,18 +59,22 @@ size_t	charset(char	*buf, char	c, size_t	l)
 
 void	mkput(char	*buf, t_block	*b)
 {
-	size_t	i;
-
-	i = 0;
 	if (b->direct != _left)
-		buf += charset(buf, ' ', b->spase);
-	ft_memcpy(buf, b->sing, ft_strlen(b->sing));
-	buf += ft_strlen(b->sing);
-	buf += charset(buf, '0', b->zero);
+	{
+		ft_memset(buf, ' ', b->spase);
+		buf += b->spase;
+	}
+	ft_memcpy(buf, b->sing, b->singl);
+	buf += b->singl;
+	ft_memset(buf, '0', b->zero);
+	buf += b->zero;
 	ft_memcpy(buf, b->nums, b->numl);
 	buf += b->numl;
 	if (b->direct == _left)
-		buf += charset(buf, ' ', b->spase);
+	{
+		ft_memset(buf, ' ', b->spase);
+		buf += b->spase;
+	}
 	ft_memcpy(buf, b->fmts, b->fmtl);
 	return ;
 }
