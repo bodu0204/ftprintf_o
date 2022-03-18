a69ba899ecf4a806d01ab028bdc61e72e0f41726
呂明 <ryoakira@ryoakiranoMacBook-Pro.local>
2022-03-18 22:49:54 +0900

    commit form makefile

--- src_bonus/tool_bonus.c
+++ src_bonus/tool_bonus.c
@@ -3,14 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   tool_bonus.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: blyu <blyu@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: ryoakira <ryoakira@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/16 17:48:55 by blyu              #+#    #+#             */
-/*   Updated: 2022/03/18 12:01:28 by blyu             ###   ########.fr       */
+/*   Updated: 2022/03/18 22:49:34 by ryoakira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mk_ft_printf_bonus.h"
+#include "debug.h"/* test */
 
 void	strupper(int	i, ...)
 {
@@ -46,22 +47,30 @@ size_t	blclen(t_block	*b)
 
 void	mkput(char	*buf, t_block	*b)
 {
+write(1, "[", 1);/* test */
 	if (b->direct != _left)
 	{
+for (size_t i = 0; i < b->spase; i++) write(1, " ", 1);/* test */
 		ft_memset(buf, ' ', b->spase);
 		buf += b->spase;
 	}
+write(1, b->sing, b->singl);/* test */
 	ft_memcpy(buf, b->sing, b->singl);
 	buf += b->singl;
+for (size_t i = 0; i < b->zero; i++) write(1, "0", 1);/* test */
 	ft_memset(buf, '0', b->zero);
 	buf += b->zero;
+write(1, b->nums, b->numl);/* test */
 	ft_memcpy(buf, b->nums, b->numl);
 	buf += b->numl;
 	if (b->direct == _left)
 	{
+for (size_t i = 0; i < b->spase; i++) write(1, " ", 1);/* test */
 		ft_memset(buf, ' ', b->spase);
 		buf += b->spase;
 	}
+write(1, b->fmts, b->fmtl);/* test */
 	ft_memcpy(buf, b->fmts, b->fmtl);
+write(1, "[", 1);/* test */
 	return ;
 }
