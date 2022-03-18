1198c944e9c1195a28d15a977e27252a0186898b
呂明 <ryoakira@ryoakiranoMacBook-Pro.local>
2022-03-18 09:21:25 +0900

    commit form makefile

--- tester/test.c
+++ tester/test.c
@@ -7,11 +7,11 @@ int main (void)
 	char *s = NULL;
 
 	printf("ori\n");
-	i = printf("apple, %%+10d = %+10d, %%-10.80s = %-10.80s, %%#x = %#x, %%#x = %#x, %#5%\n", 63, s, 1024, 0);/*  */
+	i = printf("apple, %%+10d = %+10d, %%-10.80s = %-10.80s, %%#X = %#X, %%#X = %#X, %#5%\n", 63, s, 1024, 0);/*  */
 	printf("\nreturn = %d\n", i);
 	printf("\n");
 	printf("your\n");
-	i = ft_printf("apple, %%+10d = %+10d, %%-10.80s = %-10.80s, %%#x = %#x, %%#x = %#x, %#5%\n", 63, s, 1024, 0);/*  */
+	i = ft_printf("apple, %%+10d = %+10d, %%-10.80s = %-10.80s, %%#X = %#X, %%#X = %#X, %#5%\n", 63, s, 1024, 0);/*  */
 	printf("\nreturn = %d\n", i);
 	return (0);
 }
