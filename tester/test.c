35378249f0fea8c88cad2f9aeed805e6889b3239
Bodu Lyu <blyu@c1r29s9.42tokyo.jp>
2022-03-19 13:05:10 +0900

    commit form makefile

--- tester/test.c
+++ tester/test.c
@@ -10,7 +10,7 @@ int main (void)
 	printf("ori\n");
 
 	printf("[start]");
-	i = printf(" %c %c %c ", 0, '1', '2');/*  */
+	i = printf(" %c %c %c ", '0', 0, '1');/*  */
 	printf("[end]");
 
 	printf("\nreturn = %d\n", i);
@@ -18,7 +18,7 @@ int main (void)
 	printf("your\n");
 
 	printf("[start]"); fflush(stdout);
-	i = ft_printf(" %c %c %c ", 0, '1', '2');/*  */
+	i = ft_printf(" %c %c %c ", '0', 0, '1');/*  */
 	printf("[end]");
 
 	printf("\nreturn = %d\n", i);
