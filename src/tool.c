ec5034b4f5c476ee1bed1d9782bd4815a524763c
Bodu Lyu <blyu@c1r29s9.42tokyo.jp>
2022-03-19 12:43:59 +0900

    commit form makefile

--- src/tool.c
+++ src/tool.c
@@ -6,7 +6,7 @@
 /*   By: blyu <blyu@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/16 17:48:55 by blyu              #+#    #+#             */
-/*   Updated: 2022/03/18 12:56:58 by blyu             ###   ########.fr       */
+/*   Updated: 2022/03/19 12:42:46 by blyu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,12 +39,13 @@ size_t	blclen(t_block	*b)
 	len = b->spase;
 	len += b->singl;
 	len += b->zero;
-	len += b->numl;
+	if (!(b->type == 'c' && !*(b->nums)))
+		len += b->numl;
 	len += b->fmtl;
 	return (len);
 }
 
-void	mkput(char	*buf, t_block	*b)
+size_t	mkput(char	*buf, t_block	*b)
 {
 	if (b->direct != _left)
 	{
@@ -55,13 +56,18 @@ void	mkput(char	*buf, t_block	*b)
 	buf += b->singl;
 	ft_memset(buf, '0', b->zero);
 	buf += b->zero;
-	ft_memcpy(buf, b->nums, b->numl);
-	buf += b->numl;
+	if (!(b->type == 'c' && !*(b->nums)))
+	{
+		ft_memcpy(buf, b->nums, b->numl);
+		buf += b->numl;
+	}
 	if (b->direct == _left)
 	{
 		ft_memset(buf, ' ', b->spase);
 		buf += b->spase;
 	}
 	ft_memcpy(buf, b->fmts, b->fmtl);
-	return ;
+	if (b->type == 'c' && !*(b->nums))
+		return(1);
+	return (0);
 }
