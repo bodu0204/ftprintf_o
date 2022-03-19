3747f0448af6e6af73f14aac0278af978f01f59b
Bodu Lyu <blyu@c1r29s9.42tokyo.jp>
2022-03-19 12:40:05 +0900

    commit form makefile

--- src_bonus/tool_bonus.c
+++ src_bonus/tool_bonus.c
@@ -6,7 +6,7 @@
 /*   By: blyu <blyu@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/16 17:48:55 by blyu              #+#    #+#             */
-/*   Updated: 2022/03/19 12:26:14 by blyu             ###   ########.fr       */
+/*   Updated: 2022/03/19 12:38:34 by blyu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,7 +39,7 @@ size_t	blclen(t_block	*b)
 	len = b->spase;
 	len += b->singl;
 	len += b->zero;
-	if (b->type != 'c' && *(b->nums))
+	if (!(b->type == 'c' && !*(b->nums)))
 		len += b->numl;
 	len += b->fmtl;
 	return (len);
@@ -56,7 +56,7 @@ size_t	mkput(char	*buf, t_block	*b)
 	buf += b->singl;
 	ft_memset(buf, '0', b->zero);
 	buf += b->zero;
-	if (b->type != 'c' && *(b->nums))
+	if (!(b->type == 'c' && !*(b->nums)))
 	{
 		ft_memcpy(buf, b->nums, b->numl);
 		buf += b->numl;
